b7bed78cd195c60b16cd8464e92e25e9f565a87e
Dan Phillips <danmakenoise@gmail.com>
2017-01-02 14:14:57 -0500

    extract proptypes to separate file

--- src/react-jw-player.jsx
+++ src/react-jw-player.jsx
@@ -1,34 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
-const propTypes = {
-  className: PropTypes.string,
-  onAdPlay: PropTypes.func,
-  onAdResume: PropTypes.func,
-  onEnterFullScreen: PropTypes.func,
-  onExitFullScreen: PropTypes.func,
-  onMute: PropTypes.func,
-  onUnmute: PropTypes.func,
-  onAutoStart: PropTypes.func,
-  onResume: PropTypes.func,
-  onPlay: PropTypes.func,
-  generatePrerollUrl: PropTypes.func,
-  onError: PropTypes.func,
-  onClose: PropTypes.func,
-  playerId: PropTypes.string.isRequired,
-  playlist: PropTypes.string.isRequired,
-  onReady: PropTypes.func,
-  onAdPause: PropTypes.func,
-  onPause: PropTypes.func,
-  onVideoLoad: PropTypes.func,
-  playerScript: PropTypes.string.isRequired,
-  onOneHundredPercent: PropTypes.func,
-  onThreeSeconds: PropTypes.func,
-  onTenSeconds: PropTypes.func,
-  onThirtySeconds: PropTypes.func,
-  onFiftyPercent: PropTypes.func,
-  onNinetyFivePercent: PropTypes.func,
-  muted: PropTypes.bool
-};
+import propTypes from './prop-types';
 
 const noOp = () => {};
 const defaultProps = {
