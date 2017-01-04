2fa434463716df006b28b0cd898647be9f42c714
Dan Phillips <danmakenoise@gmail.com>
2017-01-03 23:09:04 -0500

    cleanup unused code from extractions

--- src/react-jw-player.jsx
+++ src/react-jw-player.jsx
@@ -71,35 +71,6 @@ class JWPlayer extends Component {
       existingScript.onload = curriedOnLoad;
     }
   }
-  onFullScreen(event) {
-    if (event.fullscreen) {
-      this.props.onEnterFullScreen(event);
-    } else {
-      this.props.onExitFullScreen(event);
-    }
-  }
-  onMute(event) {
-    if (event.mute) {
-      this.props.onMute(event);
-    } else {
-      this.props.onUnmute(event);
-    }
-  }
-  onPlay(event) {
-    if (event.playReason === 'autostart') {
-      this.setState({
-        hasPlayed: true
-      });
-      this.props.onAutoStart(event);
-    } else if (this.state.hasPlayed && event.oldstate === 'paused') {
-      this.props.onResume(event);
-    } else {
-      this.props.onPlay(event);
-      this.setState({
-        hasPlayed: true
-      });
-    }
-  }
   setupPreroll(playerInstance) {
     playerInstance.on('beforePlay', () => {
       const currentVideo = playerInstance.getPlaylistItem();
