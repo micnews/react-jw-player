87b38e7a7620288977898050cc61f44079545b73
kevinkiklee <kevin.kik.lee@gmail.com>
2017-07-26 17:03:23 -0400

    Simplify onTime event handler

--- src/create-event-handlers/on-time.js
+++ src/create-event-handlers/on-time.js
@@ -3,67 +3,51 @@ function onTime(event) {
   const { duration, position } = event;
   const currentPositionInteger = Math.floor(position);
 
-  let shouldUpdateState = false;
-
   if (previousPositionInteger === currentPositionInteger) {
     return;
-  } else if (!hasFired.zeroSecond && currentPositionInteger === 0) {
-    this.props.onEverySecond(0);
-    hasFired.zeroSecond = true;
-    shouldUpdateState = true;
-  } else {
-    this.props.onEverySecond(currentPositionInteger);
-    shouldUpdateState = true;
   }
+
+  this.props.onEverySecond(currentPositionInteger);
 
   if (!hasFired.threeSeconds && currentPositionInteger >= 3) {
     this.props.onThreeSeconds();
     hasFired.threeSeconds = true;
-    shouldUpdateState = true;
   }
 
   if (!hasFired.tenSeconds && currentPositionInteger >= 10) {
     this.props.onTenSeconds();
     hasFired.tenSeconds = true;
-    shouldUpdateState = true;
   }
 
   if (!hasFired.thirtySeconds && currentPositionInteger >= 30) {
     this.props.onThirtySeconds();
     hasFired.thirtySeconds = true;
-    shouldUpdateState = true;
   }
 
   if (!hasFired.twentyFivePercent && ((position / duration) * 100) >= 25) {
     this.props.onTwentyFivePercent();
     hasFired.twentyFivePercent = true;
-    shouldUpdateState = true;
   }
 
   if (!hasFired.fiftyPercent && ((position / duration) * 100) >= 50) {
     this.props.onFiftyPercent();
     hasFired.fiftyPercent = true;
-    shouldUpdateState = true;
   }
 
   if (!hasFired.seventyFivePercent && ((position / duration) * 100) >= 75) {
     this.props.onSeventyFivePercent();
     hasFired.seventyFivePercent = true;
-    shouldUpdateState = true;
   }
 
   if (!hasFired.ninetyFivePercent && ((position / duration) * 100) >= 95) {
     this.props.onNinetyFivePercent();
     hasFired.ninetyFivePercent = true;
-    shouldUpdateState = true;
   }
 
-  if (shouldUpdateState) {
-    this.setState({
-      hasFired,
-      previousPositionInteger: currentPositionInteger,
-    });
-  }
+  this.setState({
+    hasFired,
+    previousPositionInteger: currentPositionInteger,
+  });
 }
 
 export default onTime;
