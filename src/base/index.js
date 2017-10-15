24abf7f05835a0d1bd2c31be6bcf560887a8624e
Dan Phillips <danmakenoise@gmail.com>
2017-10-15 18:50:50 -0400

    add in shouldComponentUpdate()

--- src/base/index.js
+++ src/base/index.js
@@ -1,20 +1,38 @@
 /* flow */
 /* eslint-disable react/no-danger */
-import React from 'react';
+import React, { Component } from 'react';
 
 type PropsType = {
+  config: {
+    playlist?: string,
+  },
   className?: string,
   playerId: string,
 };
 
-const ReactJWPlayerBase = ({ className, playerId }: PropsType) => (
-  <div
-    className={className}
-    dangerouslySetInnerHTML={{
-      __html: `<div id="${playerId}"></div>`,
-    }}
-  />
-);
+class ReactJWPlayerBase extends Component {
+  shouldComponentUpdate(newProps: PropsType) {
+    const oldConfig = this.props.config;
+    const newConfig = newProps.config;
+
+    const playlistHasChanged = oldConfig.playlist !== newConfig.playlist;
+    const fileHasChanged = oldConfig.file !== newConfig.file;
+
+    return playlistHasChanged || fileHasChanged;
+  }
+  props: PropsType;
+  render() {
+    const { className, playerId } = this.props;
+    return (
+      <div
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: `<div id="${playerId}"></div>`,
+        }}
+      />
+    );
+  }
+}
 
 ReactJWPlayerBase.defaultProps = {
   className: null,
--- src/base/index.test.js
+++ src/base/index.test.js
@@ -34,3 +34,90 @@ test('<ReactJWPlayerBase> with a supplied class', t => {
 
   t.end();
 });
+
+test('ReactJWPlayer().shouldComponentUpdate() with no change', t => {
+  const propsOne = {
+    config: {
+      file: null,
+      playlist: 'playlist',
+    },
+    playerId: 'playerId',
+  };
+
+  const propsTwo = {
+    config: {
+      file: null,
+      playlist: 'playlist',
+    },
+    playerId: 'playerId',
+  };
+
+  const { shouldComponentUpdate } = shallow(
+    <ReactJWPlayerBase {...propsOne} />,
+  ).instance();
+
+  t.notOk(
+    shouldComponentUpdate.bind({ props: propsOne })(propsTwo),
+    'it returns false when the props are the same',
+  );
+
+  t.end();
+});
+
+test('ReactJWPlayer().shouldComponentUpdate() with playlist change', t => {
+  const propsOne = {
+    config: {
+      file: null,
+      playlist: 'playlist',
+    },
+    playerId: 'playerId',
+  };
+
+  const propsTwo = {
+    config: {
+      file: null,
+      playlist: 'another playlist',
+    },
+    playerId: 'playerId',
+  };
+
+  const { shouldComponentUpdate } = shallow(
+    <ReactJWPlayerBase {...propsOne} />,
+  ).instance();
+
+  t.ok(
+    shouldComponentUpdate.bind({ props: propsOne })(propsTwo),
+    'it returns true when the playlist has changed',
+  );
+
+  t.end();
+});
+
+test('ReactJWPlayer().shouldComponentUpdate() with file change', t => {
+  const propsOne = {
+    config: {
+      file: 'file',
+      playlist: null,
+    },
+    playerId: 'playerId',
+  };
+
+  const propsTwo = {
+    config: {
+      file: 'different file',
+      playlist: null,
+    },
+    playerId: 'playerId',
+  };
+
+  const { shouldComponentUpdate } = shallow(
+    <ReactJWPlayerBase {...propsOne} />,
+  ).instance();
+
+  t.ok(
+    shouldComponentUpdate.bind({ props: propsOne })(propsTwo),
+    'it returns true when the file has changed',
+  );
+
+  t.end();
+});
